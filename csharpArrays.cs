59e2afc321b4dabf2b7a8fd6251b2e191988a072
AngeloCrescencio <42455436+AngeloCrescencio@users.noreply.github.com>
2023-05-13 21:58:54 +0000

    Initial commit

---
+++ csharpArrays.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+partial class Program {
+
+    static void CSharpArrays()
+    {
+      Console.WriteLine("CSharpArrays");
+
+      int[] notas;
+      notas = new int[2];
+      notas[0] = 9;
+      notas[1] = 10;
+      Console.WriteLine($"Nota 1 : {notas[0]} Nota 2 : {notas[1]}");
+
+      int[] valores = new int[4];
+      valores[0] = 90;
+      valores[1] = 100;
+      valores[2] = 110;
+      valores[3] = 120;
+
+      for (int i = 0; i < valores.Length; i++) {
+        Console.WriteLine($"Valor {i} : {valores[i]}");
+      }
+   }
+}
---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        CSharpArrays();
+    }
+}
