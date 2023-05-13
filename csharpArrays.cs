832f4db2b2ae1a71024676fd7ff054e603b90b09
AngeloCrescencio <42455436+AngeloCrescencio@users.noreply.github.com>
2023-05-13 22:04:52 +0000

    commit inicial

--- csharpArrays.cs
+++ csharpArrays.cs
@@ -1,26 +1,29 @@
 using System;
 using System.Text;
 
-partial class Program {
+partial class Program
+{
 
+    //testando se salva no replit.com  
     static void CSharpArrays()
     {
-      Console.WriteLine("CSharpArrays");
+        Console.WriteLine("CSharpArrays");
 
-      int[] notas;
-      notas = new int[2];
-      notas[0] = 9;
-      notas[1] = 10;
-      Console.WriteLine($"Nota 1 : {notas[0]} Nota 2 : {notas[1]}");
+        int[] notas;
+        notas = new int[2];
+        notas[0] = 9;
+        notas[1] = 10;
+        Console.WriteLine($"Nota 1 : {notas[0]} Nota 2 : {notas[1]}");
 
-      int[] valores = new int[4];
-      valores[0] = 90;
-      valores[1] = 100;
-      valores[2] = 110;
-      valores[3] = 120;
+        int[] valores = new int[4];
+        valores[0] = 90;
+        valores[1] = 100;
+        valores[2] = 110;
+        valores[3] = 120;
 
-      for (int i = 0; i < valores.Length; i++) {
-        Console.WriteLine($"Valor {i} : {valores[i]}");
-      }
-   }
+        for (int i = 0; i < valores.Length; i++)
+        {
+            Console.WriteLine($"Valor {i} : {valores[i]}");
+        }
+    }
 }
--- main.cs
+++ main.cs
@@ -3,6 +3,7 @@ using System.Text;
 
 partial class Program
 {
+    //testando se salva no replit.com
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
